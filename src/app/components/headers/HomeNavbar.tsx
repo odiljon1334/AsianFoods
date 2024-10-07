977e3540f1f2d8720c84cea9bd3e00b99d0ae644
odiljon1334 <odil1334@gmail.com>
2024-10-08 00:34:30 +0500

    feat: publish otherNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 export function HomeNavbar() {
-    const authMember = null;
+    const authMember = true;
     return (
     <div className="home-navbar">
         <Container className="navbar-container">
