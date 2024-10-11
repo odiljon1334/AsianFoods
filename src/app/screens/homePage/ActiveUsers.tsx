9e09d1dff446d2ac364583e3a3752030f06d60b7
odiljon1334 <odil1334@gmail.com>
2024-10-11 22:29:25 +0500

    feat: develop productPage nested routing system

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -15,35 +15,35 @@ const activeUsers = [
 
 
 export default function ActiveUsers() {
+  return (
+  <div className={"active-users-frame"}>
+  <Container>
+    <Stack className={"main"}>
+      <Box className={"category-title"}>Active Users</Box>
+    <Stack className={"cards-frame"}>
+  <CssVarsProvider>
+    {activeUsers.length !== 0 ? (
+    activeUsers.map((ele, index) => {
     return (
-        <div className={"active-users-frame"}>
-            <Container>
-                <Stack className={"main"}>
-                    <Box className={"category-title"}>Active Users</Box>
-                   <Stack className={"cards-frame"}>
-                    <CssVarsProvider>
-                       {activeUsers.length !== 0 ? (
-                         activeUsers.map((ele, index) => {
-                            return (
-                                <Card key={index} variant="outlined" className={"card"}>
-                                    <CardOverflow>
-                                        <AspectRatio ratio="1">
-                                        <img src={ele.memberImage} alt="" />
-                                        </AspectRatio>
-                                    </CardOverflow>
-                                    <Typography className={"member-nickname"}>
-                                        {ele.memberNick}
-                                    </Typography>
-                                </Card>
-                            )
-                        })
-                       ) : (
-                        <Box className={"no-data"}>No Active Users!</Box>
-                       )}
-                    </CssVarsProvider>
-                   </Stack>
-                </Stack>
-            </Container>
-        </div>
+      <Card key={index} variant="outlined" className={"card"}>
+        <CardOverflow>
+          <AspectRatio ratio="1">
+            <img src={ele.memberImage} alt="" />
+          </AspectRatio>
+        </CardOverflow>
+          <Typography className={"member-nickname"}>
+            {ele.memberNick}
+          </Typography>
+      </Card>
+          )
+        })
+      ) : (
+       <Box className={"no-data"}>No Active Users!</Box>
+      )}
+  </CssVarsProvider>
+    </Stack>
+    </Stack>
+  </Container>
+  </div>
     )
 }
---
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -0,0 +1,5 @@
+export default function ChosenProduct() {
+    return (
+        <div>ChosenProduct</div>
+    )
+}
