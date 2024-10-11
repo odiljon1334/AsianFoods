3c98a26cd2466f3f038df7b10cc44c42b991a6bb
odiljon1334 <odil1334@gmail.com>
2024-10-11 08:46:12 +0500

    feat: develop advertisement sectional component

--- src/app/screens/homePage/Advertisment.tsx
+++ src/app/screens/homePage/Advertisment.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 export default function Advertisment() {
     return (
-        <div>
-            Advertisment
+        <div className="ads-restaurant-frame">
+            <video 
+            className={"ads-video"}
+            autoPlay={true}
+            loop
+            muted
+            playsInline
+            data-video-media=""
+            >
+                <source type="video/mp4" src="video/burak-ads.mp4" />
+            </video>
         </div>
     )
 }
