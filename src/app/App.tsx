f202d86b8deae904e856c1f7ef60d0b66a3cb569
odiljon1334 <odil1334@gmail.com>
2024-10-30 14:07:23 +0500

    fix: modify homePage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,8 +11,6 @@ import HelpPage from './screens/helpPage';
 import '../css/app.css';
 import "../css/navbar.css";
 import "../css/footer.css";
-import Test from "./screens/Test";
-
 
 function App() {
   const location = useLocation();
@@ -33,8 +31,7 @@ function App() {
             <HelpPage />
           </Route>
           <Route path="/">
-            {/*<HomePage />*/}
-            <Test />
+            <HomePage />
           </Route>
         </Switch>
         <Footer />
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,14 +1,24 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export default function HomeNavbar() {
     const authMember = null;
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState<number>(0);
+    const [value, setValue] = useState<boolean>(true)
 
-    const buttonHandler = ( ) => {
+    useEffect(() => {
+        console.log("componentDidMount");
         setCount(count + 1);
+
+        return () => {
+            console.log("componentWillUnmount");
+        }
+    }, [value]); // array dp
+    /** HANDLERS **/
+    const buttonHandler = ( ) => {
+        setValue(!value);
     }
     return (
     <div className="home-navbar">
@@ -16,7 +26,7 @@ export default function HomeNavbar() {
             <Stack className="menu">
                 <Box>
                     <NavLink to={'/'}>
-                        <img className="brand-logo" src="/icons/burak.svg" />
+                        <img className="brand-logo" src="/icons/burak.svg"  alt={'brand-logo'}/>
                     </NavLink>
                 </Box>
 
