43b06bec0917ad446774956fa34b3c003f01551a
odiljon1334 <odil1334@gmail.com>
2024-11-02 16:41:39 +0500

    feat: create homePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -9,6 +9,11 @@ import "../../../css/home.css";
 
 
 export default function HomePage() {
+    useEffect(() => {
+        // Backend server data request => Data
+
+        // Slice: Data => Store
+    }, []);
     return (
       <div className={"homepage"}>
         <Statistics/>
--- src/lib/types/screen.ts
+++ src/lib/types/screen.ts
@@ -1,15 +1,18 @@
 import {Product} from "./product";
 import {Member} from "./member";
 
-
 /** REACT APP STATE **/
 export interface AppRootState {
     homePage: HomePageState;
 }
 
-/** HOMEPAGE **/
+/** HOMEPAGE PAGE **/
 export interface HomePageState {
     popularDishes: Product[];
     newDishes: Product[];
     topUsers: Member[];
 }
+
+/** PRODUCTS PAGE **/
+
+/** ORDERS PAGE **/
