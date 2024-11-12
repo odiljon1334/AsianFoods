c66a3dfdc846189b890ea2f123819a8a97a14064
odiljon1334 <odiljon9507@gmail.com>
2024-11-12 21:08:57 +0500

    feat: develop Basket business logic

--- src/app/services/OrderService.ts
+++ src/app/services/OrderService.ts
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { serverApi } from "../../lib/config";
+import { Order, OrderItemInput } from "../../lib/types/order";
+import { CartItem } from "../../lib/types/search";
 
 
 class OrderService {
@@ -7,6 +10,29 @@ class OrderService {
     constructor() {
         this.path = serverApi;
     }
+
+    public async createOrder(input: CartItem[]): Promise<Order> {
+        try {
+            const orderItem: OrderItemInput[] = input.map((cartItem: CartItem) => {
+                return {
+                    itemQuantity: cartItem.quantity,
+                    itemPrice: cartItem.price,
+                    productId: cartItem._id,
+                };
+            });
+
+            const url = this.path + "/order/create";
+            const result = await axios.post(url, orderItem, {
+                withCredentials: true,
+            });
+            console.log("ordersItem:", result);
+
+            return result.data;
+        } catch (err) {
+            console.log("Error, createOrder", err);
+            throw err;
+        }
+    } 
 }
 
 export default OrderService;
